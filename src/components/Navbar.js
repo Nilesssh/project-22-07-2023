083e020f324fdd0a80c6aeca478c539234753570
Nilesh <nilesh30111997@gmail.com>
2023-07-22 12:52:38 +0530

    nice

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export default function Navbar() {
   return (
-        <nav>
-        <a className="navbar-brand" href="http://localhost:3000">
-        <img
-    src="./logo.png"
-    alt="Logo"
-    width={30}
-    height={24}
-  />
-</a>
-
-        </nav>
-  )
+    <nav>
+      <a className="navbar-brand" href="http://localhost:3000">
+        <img src="./logo.png" alt="Logo" width={30} height={24} />
+      </a>
+    </nav>
+  );
 }
